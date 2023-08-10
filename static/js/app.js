98c0cc032339e861449fe9927d3448a16a6261b2
aliciahlavac <alicia.hlavac@gmail.com>
2023-08-10 18:16:05 -0500

    Added comments

--- static/js/app.js
+++ static/js/app.js
@@ -24,12 +24,18 @@ d3.json(url).then(data => {
 
         // Update the horizontal bar chart
         const barTrace = {
+            // Get the top 10 sample_values and put them in reverse order for the bar plot
             x: selectedData.sample_values.slice(0, 10).reverse(),
+            // Get the top 10 otu_ids, put them in reverse order and set them to a string with OTU in front of it
             y: selectedData.otu_ids.slice(0, 10).reverse().map(id => `OTU ${id}`),
+            // Make otu_labels the haver values
             text: selectedData.otu_labels.slice(0, 10).reverse(),
+            // Set type of chart to bar
             type: "bar",
+            // Make it horizontal
             orientation: "h"
         };
+        // Set the layout of the bar plot and plot it
         const barLayout = {
             title: `Top 10 OTUs for Sample ${selectedSample}`
         };
@@ -37,46 +43,74 @@ d3.json(url).then(data => {
 
         // Update the bubble chart
         const bubbleTrace = {
+            // Choose otu_ids for x value
             x: selectedData.otu_ids,
+            // Choose sample values as y values
             y: selectedData.sample_values,
+            // Have hover text equal to otu_labels
             text: selectedData.otu_labels,
+            // Display data points as individual markers
             mode: 'markers',
+            // Set appearance of markers
             marker: {
+                // Size is proportional to sample_values
                 size: selectedData.sample_values,
+                // Color is related to otu_ids
                 color: selectedData.otu_ids,
+                // Color scale to use is Earth
                 colorscale: 'Earth'
             }
         };
+        // Set the layout of the bubble chart
         const bubbleLayout = {
+            // Set title
             title: `Bubble Chart for Sample ${selectedSample}`,
+            // Set x axis label
             xaxis: { title: "OTU IDs" },
+            // Set y axis label
             yaxis: { title: "Sample Values" }
         };
         Plotly.newPlot("bubble", [bubbleTrace], bubbleLayout);
 
         // Display sample metadata
+       // Return item if item id matches selectedSample
         const selectedMetadata = metadata.find(item => item.id.toString() === selectedSample);
+        // Get the sample data/demographic information to display 
         const metadataPanel = d3.select("#sample-metadata");
+        // Clear previous content
         metadataPanel.html("");
-
+        
+        // Run through each key-value pair to find the demographic info
         Object.entries(selectedMetadata).forEach(([key, value]) => {
             metadataPanel.append("p").text(`${key}: ${value}`);
         });
 
         // Update the gauge chart
+        // Find the weekly wahsing frequency
         const weeklyWashingFrequency = selectedMetadata.wfreq;
 
+        // Set parameters for gauge chart
         const gaugeTrace = {
+            // Set value of gauge meter
             value: weeklyWashingFrequency,
+            // Set title
             title: { text: "Weekly Washing Frequency" },
+            // Set type of gauge chart
             type: "indicator",
+            // Set the mode
             mode: "gauge+number",
+            // Modify the guage meter
             gauge: {
+                // Range from 0 to 9
                 axis: { range: [0, 9] },
+                // Set bar color
                 bar: { color: "dark green" },
+                // Set background color
                 bgcolor: "white",
+                // Set border width and color
                 borderwidth: 2,
-                bordercolor: "gray",
+                bordercolor: "black",
+                // Set color of meter
                 steps: [
                     {range: [0, 1], color: "rgba(255, 255, 255, 0)"},
                     {range: [1, 2], color: "rgba(232, 226, 202, .5)"},
@@ -89,14 +123,15 @@ d3.json(url).then(data => {
                     {range: [8, 9], color: "rgba(50, 143, 10, 0.5)"},
                     {range: [9, 10], color: "rgba(14, 127, 0, .5)"}
                 ],
+                // Set threshold bar color and width
                 threshold: {
-                    line: { color: "red", width: 4 },
+                    line: { color: "dark green", width: 4 },
                     thickness: 0.75,
                     value: weeklyWashingFrequency
                 }
             }
         };
-
+        // Set gauge layout
         const gaugeLayout = {
             width: 400,
             height: 300,
